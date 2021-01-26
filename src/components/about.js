574f8ac2bb6f32b6bad34baba2a59b90d2ccef05
Astrid Varga <astrid.varga@idealo.de>
2021-01-26 18:04:53 +0100

    finalize about component

--- src/components/about.js
+++ src/components/about.js
@@ -4,13 +4,76 @@ import aboutStyles from "./about.module.css"
 
 export default function About() {
   return (
-    <div className={aboutStyles.about}>
+    <section className={aboutStyles.about}>
       <p className={aboutStyles.me}>
         Hello, world. I am a Full-Stack Web developer.
       </p>
-      <div className={aboutStyles.consolewrapper}>
-        <div className={aboutStyles.console}></div>
-      </div>
-    </div>
+      <article className={aboutStyles.consolewrapper}>
+        <div className={aboutStyles.console}>
+          <span className={aboutStyles.input}> {">"} Astrid.homeBase</span>
+          <span className={aboutStyles.output}> {"=>"} "Berlin, Germany" </span>
+          <span className={aboutStyles.input}>
+            {" "}
+            {">"} Astrid.workExperience.current
+          </span>
+          <span className={aboutStyles.output}>
+            {" "}
+            {"=>"} Current Work
+            <div className={aboutStyles.details}>
+              <span>company: "Preis.de"</span>
+              <span>role: "Frontend Developer"</span>
+              <span>startDate: "Feb 2019"</span>
+            </div>
+          </span>
+          <span className={aboutStyles.input}> {">"} Astrid.interests</span>
+          <span className={aboutStyles.output}>
+            {"=>"} ["my dog Bibi", "coffee", "code", "scuba diving", "ocean",
+            "forest"]
+          </span>
+          <span className={aboutStyles.input}> {">"} Astrid.education.dev</span>
+          <span className={aboutStyles.output}>
+            {"=>"} Web Development Bootcamp
+            <div className={aboutStyles.details}>
+              <span>company: "Ironhack"</span>
+              <span>startDate: "July 2018"</span>
+              <span>endDate: "Sep 2018"</span>
+            </div>
+          </span>
+          <br />
+          <span className={aboutStyles.output}>
+            {"=>"} CODE University of Applied Sciences
+            <div className={aboutStyles.details}>
+              <span>major: "Software Engineering"</span>
+              <span>startDate: "Oct 2017"</span>
+              <span>endDate: "Dec 2020"</span>
+            </div>
+          </span>
+          <span className={aboutStyles.input}> {">"} Astrid.resume</span>
+          <span className={aboutStyles.output}>
+            {"=>"} "
+            <a
+              target="_blank"
+              className={aboutStyles.link}
+              href="astridvarga.pdf"
+            >
+              astridvarga.pdf
+            </a>
+            "
+          </span>
+          <span className={aboutStyles.input}> {">"} Astrid.email</span>
+          <span className={aboutStyles.output}>
+            {"=>"} "
+            <a
+              target="_blank"
+              className={aboutStyles.link}
+              href="mailto:astrid.varga@gmx.de"
+            >
+              astrid.varga@gmx.de
+            </a>
+            "
+          </span>
+        </div>
+      </article>
+    </section>
   )
 }
